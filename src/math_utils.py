f0badf3fae5e837d143cd89a130b8563775a001a
shreerajpatil20-tech <shreerajpatil20@gmail.com>
2026-02-22 15:01:38 +0530

    auto-heal: fix src/math_utils.py

--- src/math_utils.py
+++ src/math_utils.py
@@ -5,7 +5,7 @@ Intentionally introduce a bug here (change + to -) to trigger CI failure.
 
 
 def add(a: int, b: int) -> int:
-    return a - b  # BUG
+    return a + b
 
 
 def subtract(a: int, b: int) -> int:
